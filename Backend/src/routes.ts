a67d8b9fa02bcc58b6680585e8dfe7f20a47c60e
Caio Lemec <caio.lemec@gmail.com>
2021-10-21 15:45:04 -0300

    addind authcontext

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -8,12 +8,9 @@ import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle)
-
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
-
 router.get("/messages/last3", new Get3LastMessagesController().handle)
-
 router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
--- Frontend/src/components/LoginBox/index.tsx
+++ Frontend/src/components/LoginBox/index.tsx
@@ -1,42 +1,13 @@
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
-import { useEffect } from 'react';
+import { AuthContext } from '../../context/auth';
 
 import styles from './styles.module.scss';
-import { api } from '../../services/api';
-
-type AuthResponse = {
-    token: string;
-    user : {
-        id: string;
-        avatar_url:string;
-        name: string;
-        login: string;
-    }
-}
 
 export function LoginBox () {
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=4b22ffb07139ae3885b2`;
+    const { signInUrl, user } = useContext(AuthContext)
 
-    async function signIn(githubCode:string) {
-        const response = await api.post<AuthResponse>('authenticate', {
-            code: githubCode,
-        })
-
-        const { token, user } = response.data;
-
-        localStorage.setItem('@dowhile:token', token);
-        console.log(user)
-    }
-    
-    useEffect( () => {
-        const url = window.location.href;
-        const hasGithubCode = url.includes('?code=');
-        if (hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=')
-            window.history.pushState({}, '', urlWithoutCode)
-            signIn(githubCode);
-        }
-    }, [])
+    console.log(user)
 
     return (
         <div className={styles.loginBoxWrapper}>
